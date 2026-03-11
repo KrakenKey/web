fef30dfcf19e37741552f83748a7311c7cac6a06
krakenhavoc <krakenhavoc@protonmail.com>
2026-03-11 21:16:56 +0000

    fix: inline script for api docs

---
+++ public/scalar-init.js
@@ -0,0 +1,9 @@
+Scalar.createApiReference('#scalar-api-ref', {
+  url: '/openapi.json',
+  proxyUrl: 'https://proxy.scalar.com',
+  theme: 'none',
+  darkMode: true,
+  hideDarkModeToggle: true,
+  withDefaultFonts: false,
+  layout: 'modern',
+});
